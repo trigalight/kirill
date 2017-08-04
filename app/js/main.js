1d5b90cd18ac1a73178411d3b570a6343e80e742
trigalight <view68@ya.ru>
2017-08-04 22:06:11 +0200

    second commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
 	console.log("User JS");
 });
+
+$('.editable').each(function(){
+	this.contentEditable = true;
+});
+
+// функция вывода кода to textarea
+$(document).ready(function() {
+	$('select').change(function() {
+	var currentVal = $('textarea').val();
+	$('textarea').val(currentVal + $(this).val()); 
+	$('textarea').autoResize(
+		function AutoResize(){      
+ 	console.log('autoresize.jquery.js work');
+ 	});
+	$('textarea').resizable();
+	});
+});
+
